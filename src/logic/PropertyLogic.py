fa705b8a04fec7277a842c01634fed6ea31ae217
saedisjons <saedisj20@ru.is>
2021-11-30 16:24:17 +0000

    Updated PropertyLogic

--- src/logic/PropertyLogic.py
+++ src/logic/PropertyLogic.py
@@ -8,3 +8,10 @@ class PropertyAPI:
     def createProperty(self, address: str, propertyId: str, amenities: list):
         new_property = Property(address, propertyId, amenities)
         return self.propertyRepo.save(new_property)
+
+    def findProperty(self) -> list:
+        return self.propertyRepo.find()
+    
+    def deleteProperty(self, propertyId) -> list:
+        return self.propertyRepo.delete(propertyId)
+
