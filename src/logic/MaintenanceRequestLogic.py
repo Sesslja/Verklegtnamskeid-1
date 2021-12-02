625c19a00f1805c0170cc7fa735e425615b40f23
Daníel Hugi <danielmm20@ru.is>
2021-12-02 15:52:32 +0000

    maintenance request breytt í logic layer

--- src/logic/MaintenanceRequestLogic.py
+++ src/logic/MaintenanceRequestLogic.py
@@ -1,38 +1,31 @@
 from model.MaintenanceRequestModel import MaintenanceRequest
 from model.userModel import User
 from data.database import DB
-import random
 
 class MaintenanceRequestAPI :
 
     def __init__(self) -> None :
         self.maintenanceRequestRepo = DB(MaintenanceRequest)
+        self.userRepo = DB(User)
 
-    def createMaintenanceRequest(self, address: str, to_do: list, occurrence: str, expert: str, priority: str, date: str, employee: str, verification_number: str) :
-        new_maintenance_request = MaintenanceRequest(address,to_do,occurrence,expert,priority,date,employee,verification_number)
+    def createMaintenanceRequest(self, address: str, to_do: list, occurrence: str, expert: str, priority: str, date: str, employee: str, verification_number: str, request_status: str) :
+        new_maintenance_request = MaintenanceRequest(address,to_do,occurrence,expert,priority,date,employee,verification_number,request_status)
         return self.maintenanceRequestRepo.save(new_maintenance_request)
-    
+
     def findMaintenanceRequest(self) -> list:
         return self.maintenanceRequestRepo.find()
 
-    def findOpenedMRequest(self) -> list : 
-        pass
-
-    def findClosedMRequest(self) -> list : 
-        pass
-
-    def findUpcomingMRequest(self) -> list : 
-        pass
-
-    def findOutstandingMRequest(self) -> list :
-        pass
-    
-    def createVerificationNumber(self) -> str :
-        pass
-
-    def findOpenedMRequest(self, request_status: str):
+    def findMRequestByStatus(self, request_status: str): #Closed, Opened, Upcoming, Outstanding
         return self.maintenanceRequestRepo.find({
             'where': {
-                'propertyId': request_status
+                'status': request_status
             }
         })
+
+    def createVerificationNumber(self) -> str :
+        used_numbers = self.maintenanceRequestRepo.find() # Find all maintenance request to see used numbers
+        num_length = len(used_numbers)
+        last_number = used_numbers[num_length]
+        last_number = int(last_number[:5])
+        new_num = last_number+1
+        return 'VB'+new_num
