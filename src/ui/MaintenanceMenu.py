c97499b3c353e195d4a7f6c8a3c4ce76e6c33f5b
kristinngg99 <kristinngg99@gmail.com>
2021-12-03 14:35:12 +0000

    maintenence veisla

--- src/ui/MaintenanceMenu.py
+++ src/ui/MaintenanceMenu.py
@@ -1,10 +1,12 @@
 from ui.BaseMenu import BaseMenu
+from logic.MaintenanceRequestLogic import MaintenanceRequestAPI
 
 class MaintenanceMenu(BaseMenu):
     def __init__(self):
         super().__init__()
 
         self.menu_title = "Maintenance Menu"
+        self.maintenanceRequestAPI = MaintenanceRequestAPI
 
         self.menu_options = {
             "1": {
@@ -49,16 +51,59 @@ class MaintenanceMenu(BaseMenu):
         }
 
     def openedMRequest(self):
-        print('show me open req')
+        open_request_list = self.maintenanceRequestAPI.getOpenedMRequest()
+        for request in open_request_list:
+            print(request)
 
     def closedMRequest(self):
-        print('show me closed req')
+        closed_request_list = self.maintenanceRequestAPI.getClosedMRequest()
+        for request in closed_request_list:
+            print(request)
 
     def upcomingMaintenance(self):
-        print('show me upcoming maint')
+        upcoming_request_list = self.maintenanceRequestAPI.upcomingMRequest()
+        for request in upcoming_request_list:
+            print(request)
 
     def createMRequest(self):
-        print('create me maint req')
+        adress = input("Enter Adress: ")
+        user_input = None
+        input_list = []
+        while user_input != "":
+            user_input = input("Enter stuff to do: ")
+            input_list.append(user_input)
+        occurrence = None
+        while occurrence == None:
+            occurrence = input("How often per year\n[0] if this is not regular")
+            try:
+                occurrence = int(occurrence)
+                if occurrence == 0:
+                    occurrence = False
+            except ValueError:
+                print("Enter an integer")
+                occurrence = None
+        priority = ""
+        while priority == None:
+            priority = (input("Priority [A][B][C]")).upper()
+            valid_priority_list = ["A","B","C"]
+            if priority not in valid_priority_list:
+                priority = None
+                print("Enter a valid priority")
+        start_date = input("Enter start date [dd.mm.yyyy]: ")
+        employee_id = ""
+        while employee_id == None:
+            employee_id = input("Enter employee id: ")
+            try:
+                employee_id = int(employee_id)
+            except ValueError:
+                employee_id = None
+                print("Enter a valid ID")
+
+        self.maintenanceRequestAPI.createMaintenanceRequest(adress, input_list, occurrence, priority, start_date, employee_id)
+        print("Maintenance Request succesfully created! ")
+
 
     def outstandingMRequest(self):
-        print('show me outstanding req')
+        outstanding_request_list = self.maintenanceRequestAPI.outstandingMRequest()
+        for request in outstanding_request_list:
+            print(request)
