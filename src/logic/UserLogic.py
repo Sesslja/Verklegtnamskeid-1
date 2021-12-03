a4f99bb7c61453434d08b59dc03ea2b8fb82e3a8
saedisjons <saedisj20@ru.is>
2021-12-03 13:30:41 +0000

    PropertyLL og UserLL voru horfin? Addaði þeim aftur inn :)

---
+++ src/logic/UserLogic.py
@@ -0,0 +1,26 @@
+from model.AddressType import Address
+from model.userModel import User
+from data.database import DB
+
+class UserAPI:
+    def __init__(self) -> None:
+        self.userRepo = DB(User)
+
+    def createEmployee(self, name: str, email: str, ssn: int, address: Address=None):
+        new_user = User(name=name, email=email, ssn=ssn, address=address)
+        return self.userRepo.save(new_user)
+
+    def findEmployees(self) -> list:
+        return self.userRepo.find()
+
+    def deleteEmployee(self, id) -> list:
+        return self.userRepo.delete(id)
+
+    def findEmployeesByCountry(self, country: str):
+        return self.userRepo.find({ 
+            'where': {
+                'address': {
+                    'country': 'Iceland'
+                }
+            }
+        })
