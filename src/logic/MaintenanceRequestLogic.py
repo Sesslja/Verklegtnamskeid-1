5231b04ed7034ca8274c0aef3a25b049dde825cc
kristinnssig <mortallighting@gmail.com>
2021-12-03 01:01:34 +0000

    Testing maintenanceRequest id numbers

--- src/logic/MaintenanceRequestLogic.py
+++ src/logic/MaintenanceRequestLogic.py
@@ -1,3 +1,4 @@
+from model.AddressType import Address
 from model.MaintenanceRequestModel import MaintenanceRequest
 from model.userModel import User
 from data.database import DB
@@ -7,7 +8,7 @@ class MaintenanceRequestAPI :
     def __init__(self) -> None :
         self.requestRepo = DB(MaintenanceRequest)
 
-    def createMaintenanceRequest(self, address: str, to_do: list, occurrence: str, priority: str, start_date: str, employeeid: int, verification_number: str) :
+    def createMaintenanceRequest(self, address: Address, to_do: list, occurrence: str, priority: str, start_date: str, employeeid: str, verification_number: str) :
         new_maintenance_request = MaintenanceRequest(address,to_do,occurrence,priority,start_date,employeeid,verification_number)
         return self.requestRepo.save(new_maintenance_request)
     
@@ -15,14 +16,14 @@ class MaintenanceRequestAPI :
         return self.requestRepo.find()
 
     def findMRequestByStatus(self, request_status: str): #Closed, Opened, Upcoming, Outstanding
-        return self.maintenanceRequestRepo.find({
+        return self.requestRepo.find({
             'where': {
                 'status': request_status
             }
         })
 
     def createVerificationNumber(self) -> str :
-        used_numbers = self.maintenanceRequestRepo.find() # Find all maintenance request to see used numbers
+        used_numbers = self.requestRepo.find() # Find all maintenance request to see used numbers
         num_length = len(used_numbers)
         last_number = used_numbers[num_length]
         last_number = int(last_number[:5])
--- src/model/MaintenanceRequestModel.py
+++ src/model/MaintenanceRequestModel.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class MaintenanceRequest(BaseModel) :
     '''Model for Maintenance Request information'''
-    def __init__(self,isOpen: bool=True, isFinished: bool = False, isClosed: bool=False, address: Address=None, to_do: list=None, occurrence: str=None, priority: str=None, start_date: datetime=None, employeeId: int=None, verification_number: str=None) -> None :
+    def __init__(self,isOpen: bool=True, isFinished: bool = False, isClosed: bool=False, address: Address=Address, to_do: list=None, occurrence: str=None, priority: str=None, start_date: datetime=None, employeeId: int=None, verification_number: str=None) -> None :
         super().__init__()
         self.isOpen = isOpen
         self.isFinished = isFinished
