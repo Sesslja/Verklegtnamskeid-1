1a9791750fb9a4fe87de0dfe68086c6cefdb201b
kristinnssig <mortallighting@gmail.com>
2021-12-06 16:57:39 +0000

    Changed comment for roomtype

--- src/model/RoomType.py
+++ src/model/RoomType.py
@@ -1,4 +1,4 @@
 class RoomType:
     def __init__(self, size: float, roomId: str) -> None:
-        self.size = size # Integer number with decimal, Ex. 11525 is 115,25
+        self.size = size # Size in m^2
         self.roomId = roomId
