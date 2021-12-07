6e8c671238b571a5521c6dd1cc8e035e8d7a6ff3
Sesslja <sesselja97@gmail.com>
2021-12-07 15:03:31 +0000

    # yfir það sem á að færa í EditEmployeeMEnu

--- src/ui/EmployeeOverviewSubMenu.py
+++ src/ui/EmployeeOverviewSubMenu.py
@@ -18,11 +18,11 @@ class EmployeeOverviewSubMenu(BaseMenu):
                 "title": "Search employee by id",
                 "function": "search_employee_by_id"
             },
+            # "3": {
+            #     "title": "Edit employee",
+            #     "function": "update_employee"
+            # },
             "3": {
-                "title": "Edit employee",
-                "function": "update_employee"
-            },
-            "4": {
                 "title": "Search employee by country",
                 "function": "find_employees_by_country"
             },
@@ -68,38 +68,38 @@ class EmployeeOverviewSubMenu(BaseMenu):
             print("No employee found")
         self.waitForKeyPress()
 
-    def update_employee(self):
-        '''option to edit employee \ngiven employee SSN'''
-        '''Býður notenda upp á að breyta eiginleikum starfsmanns\ngefið að notandi viti ssn starfsmanns'''
-        employee_id = input("Enter employee SSN: ")
-        try:
-            employee = self.userApi.findEmployeesByEmployeeId(employee_id)
-            try:
-                employee['ERROR']
-                print("Employee not found")
-            except TypeError:
-                print(employee)
-                dictionary = employee[0].__dict__
-                for i, key in enumerate(dictionary):
-                    print(f"| {key:<15}:  {(dictionary[key])}")
-                factor = input("\nSelect factor you want to change: ")
 
-        except ValueError:
-            print("No employee found")
+# Setja í Employee Edit Menu
+    # def update_employee(self):
+    #     '''option to edit employee \ngiven employee SSN'''
+    #     '''Býður notenda upp á að breyta eiginleikum starfsmanns\ngefið að notandi viti ssn starfsmanns'''
+    #     employee_id = input("Enter employee SSN: ")
+    #     try:
+    #         employee = self.userApi.findEmployeesByEmployeeId(employee_id)
+    #         try:
+    #             employee['ERROR']
+    #             print("Employee not found")
+    #         except TypeError:
+    #             print(employee)
+    #             dictionary = employee[0].__dict__
+    #             for i, key in enumerate(dictionary):
+    #                 print(f"| {key:<15}:  {(dictionary[key])}")
+    #             factor = input("\nSelect factor you want to change: ")
 
+    #     except ValueError:
+    #         print("No employee found")
 
+    #     self.userApi.updateEmployeeInfo('suhdfsuohf898f2-32f2h3f',{
+    #         'name': 'Bónus'
+    #     })
 
-        self.userApi.updateEmployeeInfo('suhdfsuohf898f2-32f2h3f',{
-            'name': 'Bónus'
-        })
-
-    def delete_employee(self):
-        employee_ssn = input("Enter employees SSN: ")
-        if self.userApi.deleteEmployee(employee_ssn) == True:
-            print("Employee deleted")
-        else:
-            print("Employee not found")
-        self.waitForKeyPress()
+    # def delete_employee(self):
+    #     employee_ssn = input("Enter employees SSN: ")
+    #     if self.userApi.deleteEmployee(employee_ssn) == True:
+    #         print("Employee deleted")
+    #     else:
+    #         print("Employee not found")
+    #     self.waitForKeyPress()
 
     def find_employees_by_country(self):
         '''Option to search for employees \ngiven country'''
@@ -121,13 +121,7 @@ class EmployeeOverviewSubMenu(BaseMenu):
             print("No employee found")
         self.waitForKeyPress()
 
-    def find_employee(self):
-        pass
-
     def find_manager(self):
-        pass
-
-    def update_employee_info(self):
         pass
 
     def find_by_attributy(self):
