3cb626499a508abe02a24c37f19cfb4695fc6e2c
Daníel Hugi <danielmm20@ru.is>
2021-12-09 22:51:08 +0000

    ekkert merkilegt

--- src/main.py
+++ src/main.py
@@ -14,16 +14,3 @@ if __name__ == "__main__" :
     main()
     pass
 
-#propApi = PropertyAPI()
-#
-#room = RoomType(11502, 'PR7991')
-#room2 = RoomType(8902, 'PR7992')
-
-#print(propApi.createProperty('husstadur', 'PP7990', ['klost','bad','eldhus'], [room, room2]))
-
-# mReq = MaintenanceRequestAPI()
-# 
-# addr = Address('Iceland', 'Reykjavik', '101', 'Bankastræti 5')
-# mReq.createMaintenanceRequest(addr, ['Borða mat', 'slá vegg', 'kaupa bíl'], 'ha', 'HIGH', 'now', '37a229db-6157-4bbe-b898-9d2abafb464d', 'VB00001')
-# 
-# mReq.createVerificationNumber()
